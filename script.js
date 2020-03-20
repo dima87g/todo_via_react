3fb41f3f3a056bb89d1cfacd2835a4d5b6b4eb78
dima87g <55344600+dima87g@users.noreply.github.com>
2020-03-21 01:41:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ class TaskList {
     }
 
     addTask() {
-        if (document.getElementById("task_input_field").value){
+        if (document.getElementById("task_input_field").value) {
             let taskText = document.getElementById("task_input_field").value;
-            document.getElementById("task_input_field").value= "";
+            document.getElementById("task_input_field").value = "";
 
             let newTask = new Task(taskText);
             this.tasks.push(newTask);
@@ -26,12 +26,20 @@ class TaskList {
     }
 
     updateDom() {
-        while (document.getElementById("tasks").firstElementChild) {
-            document.getElementById("tasks").firstElementChild.remove();
+        let tasksParent = document.getElementById("tasks");
+        let existTasks = document.getElementsByClassName("task");
+        let i = 0;
+        for (i; i < this.tasks.length; i++) {
+            if (existTasks[i]) {
+                this.tasks[i].replaceTaskNode(existTasks[i]);
+            } else {
+                tasksParent.append(this.tasks[i].createTaskNode());
+            }
         }
-
-        for (let task of this.tasks) {
-            document.getElementById("tasks").append(task.createTaskNode());
+        if (existTasks[i]) {
+            for (i; i < existTasks.length; i++) {
+                existTasks[i].remove();
+            }
         }
     }
 }
@@ -46,35 +54,56 @@ class Task {
     createTaskNode() {
         const node = this;
         let taskDiv = document.createElement("div");
-        taskDiv.setAttribute("class", "task")
+        taskDiv.setAttribute("class", "task");
         let finishButton = document.createElement("input");
         finishButton.setAttribute("type", "button");
-        finishButton.setAttribute("class", "task_finish_button")
-        if (this.status === false){
+        finishButton.setAttribute("class", "task_finish_button");
+        if (this.status === false) {
             finishButton.setAttribute("value", "Выполнено");
         } else {
             finishButton.setAttribute("value", "Не выполнено");
             taskDiv.setAttribute("class", "task finished_task");
         }
-        finishButton.onclick = function() {
+        finishButton.onclick = function () {
             taskList.finishTask(node);
         };
         let removeButton = document.createElement("input");
         removeButton.setAttribute("type", "button");
         removeButton.setAttribute("value", "Удалить");
         removeButton.setAttribute("class", "task_remove_button");
-        removeButton.onclick = function() {
+        removeButton.onclick = function () {
             taskList.removeTask(node);
         };
+        let par = document.createElement("p");
+        par.append(this.text);
+        par.setAttribute("class", "paragraph");
         taskDiv.append(finishButton);
-        taskDiv.append(this.text);
+        taskDiv.append(par);
         taskDiv.append(removeButton);
 
         return taskDiv;
     }
+
+    replaceTaskNode(existTask) {
+        const node = this;
+        let finishButton = existTask.getElementsByClassName("task_finish_button")[0];
+        let removeButton = existTask.getElementsByClassName("task_remove_button")[0];
+        existTask.getElementsByTagName("p")[0].textContent = this.text;
+        if (this.status === false) {
+            finishButton.setAttribute("value", "Выполнено");
+            existTask.setAttribute("class", "task");
+        } else {
+            finishButton.setAttribute("value", "Не выполнено");
+            existTask.setAttribute("class", "task finished_task");
+        }
+        finishButton.onclick = function () {
+            taskList.finishTask(node);
+        };
+        removeButton.onclick = function () {
+            taskList.removeTask(node);
+        };
+    }
 }
 
-let taskList = new TaskList();
 
-let data = fetch("http://127.0.0.1:5000/status");
-console.log(data.catch());
+let taskList = new TaskList();
