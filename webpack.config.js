6f2beacc813199fa600b5fe9df04b4fde1def6d0
dima87g <dima87g@gmai.com>
2021-02-19 14:36:27 +0300

    Made so that webpack create bundles in different folders dependend on development or prpduction argument given to console

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,33 @@
 const path = require('path');
+const { argv } = require('process');
+let out = null;
 
-module.exports = {
-  devtool: 'source-map',
-  entry: './static/js/main.js',
-  output: {
-    path: path.resolve(__dirname, 'static'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            cacheDirectory: true,
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    out = 'static/builds/dev';
+  } else if (argv.mode === 'production') {
+    out = 'static/builds/prod';
+  }
+  return {
+    devtool: 'source-map',
+    entry: './static/js/main.js',
+    output: {
+      path: path.resolve(__dirname, out),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              cacheDirectory: true,
+            }
           }
         }
-      }
-    ]
+      ]
+    }
   }
 };
