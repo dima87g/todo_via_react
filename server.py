fb4bd1fd8a63fe90c33b85edd0472f61ca03be65
dima87g <dima87g@gmai.com>
2021-02-20 14:56:34 +0300

    Made "save_edit_task" function work via SQLAlchemy

--- server.py
+++ server.py
@@ -45,6 +45,7 @@ routes_to_check = [
     "/change_password",
     "/user_delete",
     "/save_task",
+    "/save_edit_task",
 ]
 
 # print("Connection Pool Name - ", connection_pool.pool_name)
@@ -592,65 +593,71 @@ def save_edit_task():
     if OK = False : json = {"ok": "bool", "error_code": "int" or None,
                             "error_message": "str" or None}
     """
-    connection = None
-    cur = None
+    session = None
 
     try:
-        connection = connection_pool.get_connection()
-        cur = connection.cursor()
-
-        data = request.json
-        task_id = data['taskId']
-        task_text = data['taskText']
+        session = make_session()
 
         user_text_id = request.cookies.get('id')
         sign = request.cookies.get('sign')
+        data = request.json
+        task_id = data["taskId"]
+        task_text = data["taskText"]
 
-        if not check_cookies(user_text_id, sign):
-            response = make_response(jsonify(
-                {
-                    "ok": False, "error_code": 401,
-                    "error_message": "Disconnect"
-                }), 401)
-            response.delete_cookie("id")
-            response.delete_cookie("sign")
+        query = session.query(User).filter(User.user_text_id == user_text_id)
 
-            return response
+        user = query.first()
+        user_id = user.id
 
-        cur.execute('SELECT id FROM users WHERE user_text_id = %s',
-                    (user_text_id,))
+        query = session.query(Task).filter(Task.id == task_id, Task.user_id == user_id)
 
-        rows = cur.fetchall()
-        user_id = rows[0][0]
+        task_to_update = query.first()
 
-        cur.execute('UPDATE tasks SET text = %s WHERE id = %s AND user_id = '
-                    '%s', (task_text, task_id, user_id))
+        task_to_update.text = task_text
 
-        connection.commit()
+        session.commit()
 
-        response = make_response(jsonify(
+        response = make_response(
             {
                 "ok": True
-            }), 200)
-        response.set_cookie("id", user_text_id,
-                            max_age=int(cookies_config['MAX_AGE'])
-                            )
-        response.set_cookie("sign", sign,
-                            max_age=int(cookies_config['MAX_AGE'])
-                            )
+            }, 200
+        )
+        response.set_cookie(
+            "id", user_text_id, max_age=int(cookies_config['MAX_AGE'])
+        )
+        response.set_cookie(
+            "sign", sign, max_age=int(cookies_config['MAX_AGE'])
+        )
 
-        return jsonify({"ok": True})
-    except mysql.connector.Error as error:
-        return jsonify({'ok': False, 'error_code': error.errno,
-                        'error_message': error.msg})
+        return response
+
+    except sqlalchemy.exc.SQLAlchemyError as error:
+        session.rollback()
+        debug_print(error.args)
+
+        return jsonify(
+            {
+                "ok": False,
+                "error_code": None,
+                "error_message": "Contact admin for log checking..."
+            }
+        )
+
     except Exception as error:
-        return jsonify({'ok': False, 'error_code': None,
-                        'error_message': error.args[0]})
+        session.rollback()
+        debug_print(error.args)
+
+        return jsonify(
+            {
+                "ok": False,
+                "error_code": None,
+                "error_message": "Contact admin for log checking..."
+            }
+        )
+
     finally:
-        if cur is not None:
-            cur.close()
-        if connection is not None:
-            connection.close()
+        if session is not None:
+            session.close()
 
 
 @app.route("/delete_task", methods=["POST"])
