f52659df5a17877c29d321cf5c3a49175752255c
dima87g <dima87g@gmai.com>
2021-03-25 15:11:25 +0300

    Implemented LoggerMan class as Singleon

--- logger_man.py
+++ logger_man.py
@@ -5,52 +5,68 @@ import traceback
 
 
 class LoggerMan:
-    def __init__(self):
-        self.mutex = threading.Lock()
-        if not os.path.exists("./logs_archive"):
-            os.mkdir("logs_archive")
+    __instance = None
+    __log_file = None
+    __mutex_create = threading.Lock()
+    __mutex_log = threading.Lock()
+    __path_to_log = os.path.join(os.path.dirname(__file__), "error_log.txt")
 
-        self.path_to_log = os.path.dirname(__file__) + "/error_log.txt"
-        self.log_file = open(self.path_to_log, "a")
+    def __new__(cls):
+        with cls.__mutex_create:
+            if not cls.__instance:
+                cls.__instance = super(LoggerMan, cls).__new__(cls)
+            return cls.__instance
+
+    def __init__(self):
+        with self.__mutex_log:
+            if not self.__log_file:
+                self.__log_file = open(self.__path_to_log, "a")
 
     def log(self, exception):
         """
         :param exception: sys.exc_info() object
         """
-        self.mutex.acquire()
-        try:
-            log_size = self.check_size()
+        with self.__mutex_log:
+            try:
+                log_size = self.check_size()
 
-            if log_size > 1:
-                self.archive_log()
+                # Log file size in megabytes
+                if log_size > 1:
+                    self.archive_log()
 
-            time = datetime.datetime.now().astimezone()
-            self.log_file.write("*" * 20 + "\n")
-            self.log_file.write(time.strftime("%Y-%m-%d %H:%M:%S") + "\n")
-            traceback.print_exception(*exception, file=self.log_file)
-            self.log_file.write("\n")
-            self.log_file.flush()
-        except Exception:
-            pass
-        finally:
-            self.mutex.release()
+                time = datetime.datetime.now().astimezone()
+
+                self.__log_file.write("*" * 20 + "\n")
+                self.__log_file.write(time.strftime("%Y-%m-%d %H:%M:%S") + "\n")
+                traceback.print_exception(*exception, file=self.__log_file)
+                self.__log_file.write("\n")
+                self.__log_file.flush()
+            except Exception:
+                pass
 
     def check_size(self):
+        """
+        Check log file size in bytes and return size in megabytes
+        :return float: log file size in MB
+        """
         try:
-            log_size = os.path.getsize(self.path_to_log)
-            return float("{:.2f}".format(log_size / 1024))
+            log_size = os.path.getsize(self.__path_to_log)
+            return float("{:.2f}".format(log_size / 1024 / 1024))
         except Exception:
             pass
 
     def archive_log(self):
         try:
-            self.log_file.close()
+            self.__log_file.close()
+            if not os.path.exists("./logs_archive"):
+                os.mkdir("logs_archive")
             time = datetime.datetime.now().astimezone()
             time = time.strftime("%Y-%m-%d_%H:%M:%S")
             path_to_archive = os.path.dirname(__file__) + "/logs_archive/log_" + time + ".txt"
 
             files_in_archive = os.listdir(path="./logs_archive")
 
+            # Number of log files in archive
             if len(files_in_archive) > 3:
                 files_in_archive.sort(
                     key=lambda x: os.path.getmtime(os.path.join("logs_archive", x)), reverse=True
@@ -60,8 +76,8 @@ class LoggerMan:
 
                 os.remove(os.path.join("logs_archive", log_to_remove))
 
-            os.renames(self.path_to_log, path_to_archive)
+            os.renames(self.__path_to_log, path_to_archive)
 
-            self.log_file = open(self.path_to_log, "a")
+            self.__log_file = open(self.__path_to_log, "a")
         except Exception:
             pass
