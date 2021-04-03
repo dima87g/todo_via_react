41ac36863a633afc1134791e7f3cd856e15046a4
dima87g <dima87g@gmail.com>
2021-04-04 00:34:20 +0300

    Made test user for fast tables refresh without necessary every time register

--- alchemy_sql.py
+++ alchemy_sql.py
@@ -1,11 +1,12 @@
 import os
 import configparser
-from sqlalchemy import Column, INT, BIGINT, VARCHAR, create_engine, ForeignKey
+from sqlalchemy import Column, INT, BIGINT, VARCHAR, create_engine, ForeignKey, Sequence
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 config = configparser.ConfigParser()
-config.read(os.path.dirname(__file__) + "/server_config.ini")
+config.read(os.path.join(os.path.dirname(__file__), "server_config.ini"))
+
 db_config = config["data_base"]
 
 user = db_config["user"]
@@ -85,79 +86,100 @@ class Task(Base):
                    self.task_position, self.list_id)
 
 
-# if __name__ == "__main__":
-#     Base.metadata.drop_all(engine)
-# #
-#     Base.metadata.create_all(engine)
-#
-#     dima = User(user_text_id="dimaTextId", user_name="dima",
-#                 hashed_password="dimaHashedPassword")
-#     kman = User(user_text_id="kmanTextId", user_name="kman",
-#                 hashed_password="kmanHashedPassword")
-#
-#     session.add_all([dima, kman])
-#
-#     time.sleep(5)
-#
-#     session.commit()
-#
-#     dima_1st_list = List(user_id=dima.id, name="dima 1st list")
-#     dima_2nd_list = List(user_id=dima.id, name="dima 2nd list")
-#     kman_1st_list = List(user_id=kman.id, name="kman 1st list")
-#
-#     session.add_all([dima_1st_list, dima_2nd_list, kman_1st_list])
-#
-#     session.commit()
-#
-#     dima_1st_list_1st_task = Task(user_id=dima.id,
-#                                   text="dima 1st list 1st task", status=0,
-#                                   list_id=dima_1st_list.id)
-#     dima_1st_list_2nd_task = Task(user_id=dima.id,
-#                                   text="dima 1st list 2nd task", status=0,
-#                                   list_id=dima_1st_list.id)
-#     dima_1st_list_3rd_task = Task(user_id=dima.id,
-#                                   text="dima 1st list 3rd task", status=0,
-#                                   list_id=dima_1st_list.id)
-#     dima_1st_list_4rs_task = Task(user_id=dima.id,
-#                                   text="dima 1st list 4rs task", status=0,
-#                                   list_id=dima_1st_list.id)
-#     dima_2nd_list_1st_task = Task(user_id=dima.id,
-#                                   text="dima 2nd list 1st task", status=0,
-#                                   list_id=dima_2nd_list.id)
-#
-#     session.add_all(
-#         [
-#             dima_1st_list_1st_task,
-#             dima_1st_list_2nd_task,
-#             dima_1st_list_3rd_task,
-#             dima_1st_list_4rs_task,
-#             dima_2nd_list_1st_task
-#         ]
-#     )
-#
-#     session.commit()
-#
-#     dima_1st_list_1st_task.task_position = dima_1st_list_1st_task.id
-#     dima_1st_list_2nd_task.task_position = dima_1st_list_2nd_task.id
-#     dima_1st_list_3rd_task.task_position = dima_1st_list_3rd_task.id
-#     dima_1st_list_4rs_task.task_position = dima_1st_list_4rs_task.id
-#     dima_2nd_list_1st_task.task_position = dima_2nd_list_1st_task.id
-#     dima_1st_list_2nd_task.parent_id = dima_1st_list_1st_task.id
-#     dima_1st_list_4rs_task.parent_id = dima_1st_list_1st_task.id
-#
-#     session.add_all(
-#         [
-#             dima_1st_list_1st_task,
-#             dima_1st_list_2nd_task
-#         ]
-#     )
-#
-#     session.commit()
+if __name__ == "__main__":
+    Base.metadata.drop_all(engine)
 
+    Base.metadata.create_all(engine)
+
+    session = make_session()
+
+    test_user = User(
+        id=1,
+        user_text_id="jNDATAMOEE6AZwQCQiW1WWzaJttZoSMHZIBSyyAdEHjdlaptWoiDjl0bUSxjFbfA",
+        user_name="test",
+        hashed_password="pbkdf2:sha256:150000$IlDIHWhm$0da9b8158cf68227087c17750432cc409d8d1c5abb3f01c405bebe6f98a17fda"
+    )
+
+    test_user_list = List(
+        id=1,
+        user_id=1,
+        name="main"
+    )
+
+    session.add(test_user)
+
+    session.commit()
+
+    session.add(test_user_list)
+
+    session.commit()
+
+    # dima = User(user_text_id="dimaTextId", user_name="dima",
+    #             hashed_password="dimaHashedPassword")
+    # kman = User(user_text_id="kmanTextId", user_name="kman",
+    #             hashed_password="kmanHashedPassword")
+    #
+    # session.add_all([dima, kman])
+    #
+    # session.commit()
+    #
+    # dima_1st_list = List(user_id=dima.id, name="dima 1st list")
+    # dima_2nd_list = List(user_id=dima.id, name="dima 2nd list")
+    # kman_1st_list = List(user_id=kman.id, name="kman 1st list")
+    #
+    # session.add_all([dima_1st_list, dima_2nd_list, kman_1st_list])
+    #
+    # session.commit()
+    #
+    # dima_1st_list_1st_task = Task(user_id=dima.id,
+    #                               text="dima 1st list 1st task", status=0,
+    #                               list_id=dima_1st_list.id)
+    # dima_1st_list_2nd_task = Task(user_id=dima.id,
+    #                               text="dima 1st list 2nd task", status=0,
+    #                               list_id=dima_1st_list.id)
+    # dima_1st_list_3rd_task = Task(user_id=dima.id,
+    #                               text="dima 1st list 3rd task", status=0,
+    #                               list_id=dima_1st_list.id)
+    # dima_1st_list_4rs_task = Task(user_id=dima.id,
+    #                               text="dima 1st list 4rs task", status=0,
+    #                               list_id=dima_1st_list.id)
+    # dima_2nd_list_1st_task = Task(user_id=dima.id,
+    #                               text="dima 2nd list 1st task", status=0,
+    #                               list_id=dima_2nd_list.id)
+    #
+    # session.add_all(
+    #     [
+    #         dima_1st_list_1st_task,
+    #         dima_1st_list_2nd_task,
+    #         dima_1st_list_3rd_task,
+    #         dima_1st_list_4rs_task,
+    #         dima_2nd_list_1st_task
+    #     ]
+    # )
+    #
+    # session.commit()
+    #
+    # dima_1st_list_1st_task.task_position = dima_1st_list_1st_task.id
+    # dima_1st_list_2nd_task.task_position = dima_1st_list_2nd_task.id
+    # dima_1st_list_3rd_task.task_position = dima_1st_list_3rd_task.id
+    # dima_1st_list_4rs_task.task_position = dima_1st_list_4rs_task.id
+    # dima_2nd_list_1st_task.task_position = dima_2nd_list_1st_task.id
+    # dima_1st_list_2nd_task.parent_id = dima_1st_list_1st_task.id
+    # dima_1st_list_4rs_task.parent_id = dima_1st_list_1st_task.id
+    #
+    # session.add_all(
+    #     [
+    #         dima_1st_list_1st_task,
+    #         dima_1st_list_2nd_task
+    #     ]
+    # )
+    #
+    # session.commit()
+    #
     # print(dima)
     #
     # print(dima_1st_list)
-
+    #
     # print(f"dima lists: {dima.lists}")
     # print(f"kman lists: {kman.lists}")
     # print(kman_1st_list.user)
@@ -166,7 +188,7 @@ class Task(Base):
     # print(dima_1st_list_2nd_task.children)
     # print(dima_1st_list.tasks)
     # print(dima_2nd_list.tasks)
-
+    #
     # users = session.query(User).filter(User.user_name == "dima")
     #
     # print(users.first().hashed_password)
