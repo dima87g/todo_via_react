0c11848985c1e2c6f5a0090a9e7fb1694ea90329
dima87g <dima87g@gmail.com>
2021-04-10 21:18:50 +0300

    Working on tests......

--- tests/server_tests/test.py
+++ tests/server_tests/test.py
@@ -1,5 +1,6 @@
 import unittest
 from server import app
+from alchemy_sql import Base, engine
 
 app.testing = True
 
@@ -16,6 +17,55 @@ class MyTestCase(unittest.TestCase):
             client.set_cookie("localhost", "lang", "en")
             response = client.get("/", follow_redirects=True)
         self.assertEqual(response.status, "200 OK")
+
+
+class ApiTesting(unittest.TestCase):
+    def setUp(self) -> None:
+        Base.metadata.create_all(engine)
+
+    def tearDown(self) -> None:
+        Base.metadata.drop_all(engine)
+
+    def test_user_register_success(self):
+        with app.test_client() as client:
+            response = client.post(
+                "/user_register",
+                json={
+                    "userName": "test",
+                    "password": "Trcbjyt1"
+                },
+                follow_redirects=True
+            )
+            data = response.json
+        self.assertEqual(data["ok"], True)
+
+    def test_user_register_integrity_error(self):
+        with app.test_client() as client:
+            client.post(
+                "/user_register",
+                json={
+                    "userName": "test",
+                    "password": "12345"
+                },
+                follow_redirects=True
+            )
+
+            response = client.post(
+                "/user_register",
+                json={
+                    "userName": "test",
+                    "password": "12345"
+                },
+                follow_redirects=True
+            )
+            data = response.json
+
+            expect = {
+                "error_code": 1062,
+                "error_message": "Contact admin for log checking...",
+                "ok": False
+            }
+            self.assertEqual(data, expect)
 
 
 if __name__ == '__main__':
