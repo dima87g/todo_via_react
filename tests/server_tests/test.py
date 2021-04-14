3898b045d997b7ec1de958f49b94f3851d322c13
dima87g <dima87g@gmail.com>
2021-04-14 13:29:21 +0300

    Working on tests, target - 100% :)

--- tests/server_tests/test.py
+++ tests/server_tests/test.py
@@ -5,18 +5,18 @@ from alchemy_sql import Base, engine
 app.testing = True
 
 
-class MyTestCase(unittest.TestCase):
-    def test_main_lang_ru(self):
-        with app.test_client() as client:
-            client.set_cookie("localhost", "lang", "ru")
-            response = client.get("/", follow_redirects=True)
-        self.assertEqual(response.status, "200 OK")
-
-    def test_main_lang_en(self):
-        with app.test_client() as client:
-            client.set_cookie("localhost", "lang", "en")
-            response = client.get("/", follow_redirects=True)
-        self.assertEqual(response.status, "200 OK")
+# class MyTestCase(unittest.TestCase):
+#     def test_main_lang_ru(self):
+#         with app.test_client() as client:
+#             client.set_cookie("localhost", "lang", "ru")
+#             response = client.get("/", follow_redirects=True)
+#         self.assertEqual(response.status, "200 OK")
+#
+#     def test_main_lang_en(self):
+#         with app.test_client() as client:
+#             client.set_cookie("localhost", "lang", "en")
+#             response = client.get("/", follow_redirects=True)
+#         self.assertEqual(response.status, "200 OK")
 
 
 class ApiTesting(unittest.TestCase):
@@ -26,9 +26,131 @@ class ApiTesting(unittest.TestCase):
     def tearDown(self) -> None:
         Base.metadata.drop_all(engine)
 
-    def test_user_register_success(self):
+    # def test_user_register_success(self):
+    #     with app.test_client() as client:
+    #         response = client.post(
+    #             "/user_register",
+    #             json={
+    #                 "userName": "test",
+    #                 "password": "Trcbjyt1"
+    #             },
+    #             follow_redirects=True
+    #         )
+    #         data = response.json
+    #     self.assertEqual(data["ok"], True)
+    #
+    # def test_user_register_integrity_error(self):
+    #     with app.test_client() as client:
+    #         client.post(
+    #             "/user_register",
+    #             json={
+    #                 "userName": "test",
+    #                 "password": "12345"
+    #             },
+    #             follow_redirects=True
+    #         )
+    #
+    #         response = client.post(
+    #             "/user_register",
+    #             json={
+    #                 "userName": "test",
+    #                 "password": "12345"
+    #             },
+    #             follow_redirects=True
+    #         )
+    #         data = response.json
+    #
+    #         expect = {
+    #             "error_code": 1062,
+    #             "error_message": "Contact admin for log checking...",
+    #             "ok": False
+    #         }
+    #         self.assertEqual(data, expect)
+    #
+    # def test_user_Login_success(self):
+    #     with app.test_client() as client:
+    #         client.post(
+    #             "/user_register",
+    #             json={
+    #                 "userName": "test",
+    #                 "password": "Trcbjyt1"
+    #             },
+    #             follow_redirects=True
+    #         )
+    #         response = client.post(
+    #             "/user_login",
+    #             json={
+    #                 "userName": "test",
+    #                 "password": "Trcbjyt1"
+    #             },
+    #             follow_redirects=True
+    #         )
+    #         self.assertEqual(response.status, "200 OK")
+    #
+    # def test_user_login_not_exists(self):
+    #     with app.test_client() as client:
+    #         response = client.post(
+    #             "/user_login",
+    #             json={
+    #                 "userName": "test",
+    #                 "password": "Trcbjyt1"
+    #             },
+    #             follow_redirects=True
+    #         )
+    #         self.assertEqual(response.status, "401 UNAUTHORIZED")
+    #
+    # def test_user_login_wrong_password(self):
+    #     with app.test_client() as client:
+    #         client.post(
+    #             "/user_register",
+    #             json={
+    #                 "userName": "test",
+    #                 "password": "Trcbjyt1"
+    #             },
+    #             follow_redirects=True
+    #         )
+    #         response = client.post(
+    #             "user_login",
+    #             json={
+    #                 "userName": "test",
+    #                 "password": "trcbjyt1"
+    #             },
+    #             follow_redirects=True
+    #         )
+    #         self.assertEqual(response.status, "401 UNAUTHORIZED")
+    #
+    # def test_change_password_success(self):
+    #     with app.test_client() as client:
+    #         client.post(
+    #             "/user_register",
+    #             json={
+    #                 "userName": "test",
+    #                 "password": "Trcbjyt1"
+    #             },
+    #             follow_redirects=True
+    #         )
+    #         client.post(
+    #             "/user_login",
+    #             json={
+    #                 "userName": "test",
+    #                 "password": "Trcbjyt1"
+    #             },
+    #             follow_redirects=True
+    #         )
+    #         response = client.post(
+    #             "/change_password",
+    #             json={
+    #                 "oldPassword": "Trcbjyt1",
+    #                 "newPassword": "trcbjyt1"
+    #             },
+    #             follow_redirects=True
+    #         )
+    #         data = response.json
+    #         self.assertEqual(data["ok"], True)
+
+    def test_change_password_incorrect_password(self):
         with app.test_client() as client:
-            response = client.post(
+            client.post(
                 "/user_register",
                 json={
                     "userName": "test",
@@ -36,35 +158,29 @@ class ApiTesting(unittest.TestCase):
                 },
                 follow_redirects=True
             )
-            data = response.json
-        self.assertEqual(data["ok"], True)
-
-    def test_user_register_integrity_error(self):
-        with app.test_client() as client:
             client.post(
-                "/user_register",
+                "/user_login",
                 json={
                     "userName": "test",
-                    "password": "12345"
+                    "password": "Trcbjyt1"
                 },
                 follow_redirects=True
             )
-
             response = client.post(
-                "/user_register",
+                "/change_password",
                 json={
-                    "userName": "test",
-                    "password": "12345"
+                    "oldPassword": "trcbjyt1",
+                    "newPassword": "trcbjyt1"
                 },
                 follow_redirects=True
             )
             data = response.json
 
             expect = {
-                "error_code": 1062,
-                "error_message": "Contact admin for log checking...",
-                "ok": False
-            }
+                         "ok": False,
+                         "error_code": None,
+                         "error_message": "Your password are incorrect!"
+                     }
             self.assertEqual(data, expect)
 
 
