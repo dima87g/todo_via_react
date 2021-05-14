bb466091a3203e8477dfaac1fd1975505634b032
dima87g <dima87g@gmail.com>
2021-05-14 08:53:28 +0300

    Made "rootReducer" for reset Redux state when user is logging out

--- static/js/redux/store.js
+++ static/js/redux/store.js
@@ -4,12 +4,20 @@ import {appReducer} from "./reducers/appReducer";
 import {loginReducer} from "./reducers/loginReducer";
 import {settingsReducer} from "./reducers/settingsReducer";
 
+const rootReducer = (state, action) => {
+    if (action.type === 'USER_LOGOUT') {
+        return mainReducer(undefined, action);
+    } else {
+        return mainReducer(state, action);
+    }
+}
+
 const mainReducer = combineReducers({
     app: appReducer,
     login: loginReducer,
     settings: settingsReducer
 })
 
-const store = createStore(mainReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true, traceLimit: 25 }));
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true, traceLimit: 25 }));
 
 export {store}
